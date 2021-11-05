5f18cac5e26bb2b1f50bbdc63b58866162f49e39
insssss11 <simpson9794@korea.ac.kr>
2021-11-05 09:20:41 +0900

    Modified Errors.hpp

--- include/Errors.hpp
+++ include/Errors.hpp
@@ -6,10 +6,10 @@
 static void printError(
     const std::string cls, const std::string method, const std::string message)
 {
-    std::cerr << "-------------------------------------------------------------------" << std::endl;
+    std::cerr << "-------------------------------------------------------------------------" << std::endl;
     std::cerr << "Error in " << cls << "::" << method << "," << std::endl;
     std::cerr << "\t" << message << std::endl;
-    std::cerr << "-------------------------------------------------------------------" << std::endl;
+    std::cerr << "-------------------------------------------------------------------------" << std::endl;
 }
 
 #endif // ERRORS_HPP
