95725ebcb7fe7b4a9f56957fe7701c4ed5f95fb7
insssss11 <simpson9794@korea.ac.kr>
2022-03-18 17:45:06 +0900

    gen_gasfile executable takes arguments for penning effect if provided

--- gen_gasfile.cpp
+++ gen_gasfile.cpp
@@ -13,28 +13,38 @@ std::vector<double> MagneticFieldGrid();
 
 int main(int argc, char *argv[])
 {
-    if (argc != 6)
+    std::unique_ptr<MediumMagboltz> gas;
+    if(argc < 6 || argc > 8)
     {
-        std::cout << "Usage : gen_gasfile <arg1> <arg2> <arg3> <arg4> <arg5>" << std::endl;
+        std::cout << "Usage : gen_gasfile <arg1> <arg2> <arg3> <arg4> <arg5> [<arg6> <arg7>]" << std::endl;
         std::cout << "\t<arg1> : Ionizable gas" << std::endl;
         std::cout << "\t<arg2> : Fraction of the gas in percentage" << std::endl;
         std::cout << "\t<arg3> : Quenching gas" << std::endl;
         std::cout << "\t<arg4> : Fraction of the gas in percentage" << std::endl;
         std::cout << "\t<arg5> : Gas pressure in mmHg" << std::endl;
+        std::cout << "Optionals" << std::endl;
+        std::cout << "\t<arg6> : Penning transfer probabiliy.(optional)" << std::endl;
+        std::cout << "\t<arg7> : Mean free path of Penning effect.(optional)" << std::endl;
         return 0;
     }
+
     const char *gas1 = argv[1], *gas2 = argv[3];
     const double frac1 = atof(argv[2]), frac2 = atof(argv[4]);
     const double pressure = atof(argv[5]);
+    if(argc == 6)
+        gas = MediumMagboltzFactory::createGasMixture(gas1, frac1, gas2, frac2);
+    else if(argc == 7)
+        gas = MediumMagboltzFactory::createGasMixture(gas1, frac1, gas2, frac2, atof(argv[6]), 0.);
+    else
+        gas = MediumMagboltzFactory::createGasMixture(gas1, frac1, gas2, frac2, atof(argv[6]), atof(argv[7]));
 
     std::vector<double> eFieldGrid = ElectricFieldGrid();
     std::vector<double> bFieldGrid = MagneticFieldGrid();
     std::vector<double> aGrid = {0.};
 
-    std::unique_ptr<MediumMagboltz> gas
-        = MediumMagboltzFactory::createGasMixture(gas1, frac1, gas2, frac2);
+
     gas->SetPressure(pressure);
-    gas->SetFieldGrid(eFieldGrid, bFieldGrid, aGrid); 
+    gas->SetFieldGrid(eFieldGrid, bFieldGrid, aGrid);
     gas->GenerateGasTable(10, false);
     char gasFileName[256];
     sprintf(gasFileName, "%s_%02d_%s_%02d_%04d.gas", gas1, (int)frac1, gas2, (int)frac2, (int)pressure);
--- scripts/gen_gasfile.sh
+++ scripts/gen_gasfile.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-if [ $# -ne 7 ];then
-    echo "Usage : source gen_gasfile [arg1] [arg2] [arg3] [arg4] [arg5] [arg6] [arg7]"
+if [ $# -lt 7 -o $# -gt 9 ];then
+    echo "Usage : source gen_gasfile <arg1> <arg2> <arg3> <arg4> <arg5> <arg6> <arg7> [<arg8> <arg9>]"
     echo "Arguments list : "
-    echo "  [arg1] : name of gas1"
-    echo "  [arg2] : volume fraction of gas1(in percentage)"
-    echo "  [arg3] : name of gas2"
-    echo "  [arg4] : volume fraction of gas2(in percentage)"
-    echo "  [arg5] : pressure increment"
-    echo "  [arg6] : pressure begin"
-    echo "  [arg7] : pressure end(the end point is included)"
-    echo "This scripts generate gas file iterating with pressures increased by [arg5] starting from [arg6] to [arg7]."
+    echo "  <arg1> : name of gas1"
+    echo "  <arg2> : volume fraction of gas1(in percentage)"
+    echo "  <arg3> : name of gas2"
+    echo "  <arg4> : volume fraction of gas2(in percentage)"
+    echo "  <arg5> : pressure increment"
+    echo "  <arg6> : pressure begin"
+    echo "  <arg7> : pressure end(the end point is included)"
+    echo "Optional"
+    echo "  <arg8> : Penning transfer probability"
+    echo "  <arg9> : Mean free path of transfer"
+    echo "This scripts generate gas file iterating with pressures increased by <arg5> starting from <arg6> to <arg7>."
     return
 fi
 
 for p in $( seq -f %04g ${6} ${5} ${7} )
 do
     echo "Generating gas file ${1}_${2}_${3}_${4}_${p}.gas ..."
-    ./gen_gasfile ${1} ${2} ${3} ${4} ${p}  
+    ./gen_gasfile ${1} ${2} ${3} ${4} ${p} ${8} ${9}
 done
